7559eb5e68e2e4eacffd5576e00d9c85166a4615
Kellyan <laraj6316@gmail.com>
2025-03-28 13:35:03 -0600

    Clase Entry

---
+++ src/examen2labprogra2/Entry.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package examen2labprogra2;
+
+/**
+ *
+ * @author laraj
+ */
+public class Entry {
+
+    private String username;  
+    private long position;    
+    private Entry next;       
+
+    public Entry(String username, long position) {
+        this.username = username;
+        this.position = position;
+        this.next = null;  
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public long getPosition() {
+        return position;
+    }
+
+    public void setPosition(long position) {
+        this.position = position;
+    }
+
+    public Entry getNext() {
+        return next;
+    }
+
+    public void setNext(Entry next) {
+        this.next = next;
+    }
+    
+}
---
+++ src/examen2labprogra2/Trophy.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package examen2labprogra2;
+
+/**
+ *
+ * @author laraj
+ */
+public class Trophy {
+    
+   
+}
